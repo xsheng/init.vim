99f2c5c59ebd258a5616e1feb1cb8d333329704a
xsheng <xw.sheng@gmail.com>
2011-12-10 20:51:18 +0800

    Create the InverseLaplace class

---
+++ InverseLaplace.py
@@ -0,0 +1,13 @@
+__author__ = 'XSheng'
+__doc__ = '''
+This module is the interface of inverse laplace methods for computing ideal response of well testing.
+'''
+import numpy as np
+
+class InverseLaplace():
+    def __init__(self):
+        '''
+        Initialize the methods
+        '''
+        pass
+
