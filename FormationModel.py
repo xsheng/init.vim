c6c301d5d8fa59df63104f92123d3119268bf3bb
xsheng <xw.sheng@gmail.com>
2011-12-12 00:20:21 +0800

    Initiate the FormationModel class that implements the common formation models

--- FormationModel.py
+++ FormationModel.py
@@ -4,6 +4,7 @@ This module is the interface of formation model.
 '''
 import numpy as np
 import scipy.special as spf
+from scipy.special import iv,kn,jv
 
 class FormationModel():
 
@@ -21,7 +22,7 @@ class FormationModel():
         sqrt_u = np.sqrt(u)
         sqrt_uRd = sqrt_u*R_D
         
-        fu = sqrt_u*(spf.kn(1,sqrt_u)*spf.iv(0,sqrt_uRd) + spf.kn(0,sqrt_uRd)*spf.iv(1,sqrt_u)) /\
-             (spf.kn(1,sqrt_u)*spf.iv(0,sqrt_uRd) - spf.kn(0,sqrt_uRd)*spf.iv(0,sqrt_u))
+        fu = sqrt_u*(kn(1,sqrt_u)*iv(0,sqrt_uRd) + kn(0,sqrt_uRd)*iv(1,sqrt_u)) /\
+             (kn(1,sqrt_u)*iv(0,sqrt_uRd) - kn(0,sqrt_uRd)*iv(0,sqrt_u))
         pwd = 1/u*(1+S*fu)/(fu+C_D*u*(1+S*fu))
         return pwd
